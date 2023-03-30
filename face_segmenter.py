0a0e618315ea2d602a8087a56d99ee6ab7e72cbd
igrodfer <igrodfer@inf.upv.es>
2023-03-30 20:59:01 +0200

    first commit

---
+++ face_segmenter.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140]).astype('uint8')
+
+
+def segment_image(image,tile_size,margin_top=0,margin_left=0):
+    img_height , img_width = image.shape
+    image_no_margin = image[margin_top:,margin_left:]
+
+    img_height , img_width = image_no_margin.shape
+    
+    new_height = ((img_height)//tile_size)*tile_size
+    new_width = ((img_width)//tile_size)*tile_size
+
+    image = image[:new_height,:new_width]
+
+    tiled_array =  image.reshape((img_height) // tile_size,
+                                 tile_size,
+                                 (img_width) // tile_size,
+                                 tile_size)
+
+    tiled_array = tiled_array.swapaxes(1,2)
+
+    return np.concatenate(tiled_array,axis=0),(new_height,new_width)
